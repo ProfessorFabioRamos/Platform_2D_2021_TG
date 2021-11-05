a6abc009773b47321c06a8bf62bae7337dc46f88
Fabio Ramos <fabiocasperxan@gmail.com>
2021-11-05 08:23:28 -0300

    29-10-21

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,17 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     private Rigidbody2D rig;
+    private Animator anim;
+    private SpriteRenderer spr;
+    public float jumpForce = 6;
+    public LayerMask whatIsGround;
+
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,6 +24,24 @@ public class Player : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
 
+        if(h>0)Flip(true);
+        else if(h<0)Flip(false);
+
         rig.velocity = new Vector2(h*moveSpeed, rig.velocity.y);
+
+        anim.SetFloat("speed", Mathf.Abs(h));
+
+        bool grounded = Physics2D.OverlapCircle(transform.position, 0.2f, whatIsGround);
+
+        anim.SetBool("grounded", grounded);
+        Debug.Log(grounded);
+
+        if(Input.GetButtonDown("Jump") && grounded){
+            rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void Flip(bool faceRight){
+        spr.flipX = !faceRight;
     }
 }
